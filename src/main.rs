c2c950a2612537f8c6aadaba9312e3cc712f67c5
Nicolas AMATI <skkyz3r.dev@gmail.com>
2025-05-07 13:20:32 +0200

    Add `clone` logic

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 mod git;
 
 use clap::Parser;
-use git::commands::Gitwarp;
+use git::{commands::Gitwarp, exec::clone};
 
 fn main() {
     let args: Gitwarp = Gitwarp::parse();
 
     match args.command {
-        git::commands::GitCommand::Clone(clone) => {
-            println!("URL: {}", clone.url);
-        }
+        git::commands::GitCommand::Clone(c) => clone(&c.url).unwrap_or_else(|e| {
+            eprintln!("Error: {}", e);
+            std::process::exit(1);
+        }),
     }
 }
